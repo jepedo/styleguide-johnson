ef7b584d48feca2408dc1964a0d08a3f7737c504
umatters <andrew@usabilitymatters.com>
2016-01-06 12:16:47 -0500

    form groupings added.

--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,7 @@ gulp.task('staticStyleguide:generate', function() {
         appRoot: styleguideAppRoot,
         overviewPath: overviewPath,   
         extraHead: [
+        '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>',
   '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>'
 ]
 
